356780a93ea8cebad6987eb4d43df7927b38ec6f
1Miguel <jmjugsimar@gmail.com>
2023-04-06 21:40:16 +0800

    initial implementation - untested

--- include/u_cli.h
+++ include/u_cli.h
@@ -9,7 +9,88 @@
  * @copyright Copyright (c) 2023
  * 
  */
-#ifdef __U_CLI_H
+#ifndef __U_CLI_H
 #define __U_CLI_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+/** @addtogroup u_cli
+  * @{
+  */
+
+/* ***************************************************************************/
+/*    Public Macro Defines                                                   */
+/* ***************************************************************************/
+/* max number of commands that can be registered */
+#define MAX_NUM_COMMANDS         ( 10 )
+/* max number of characters that can be received */
+#define MAX_RECEIVE_BUFFER_CHAR  ( 100 )
+/* max number of characters that can be printed  */
+#define MAX_PRINT_BUFFER_CHAR    ( 100 )
+
+/* ***************************************************************************/
+/*    Public Type Defines                                                    */
+/* ***************************************************************************/
+/*!
+   @brief CLI command structure definition.
+ */
+typedef struct uCli_command_s
+{
+    const char* cmd;     /*!< command name                               */
+    const char* alias;   /*!< command alias name                         */
+    const char* desc;    /*!< command help description                   */
+    char*   args;    /*!< pointer to command command arguments       */
+    void ( *pCallback )( struct uCli_command_s* command, char** pArgs, uint8_t numArgs );  /*!< pointer to callback handler function */
+} uCli_command_t;
+
+/* ***************************************************************************/
+/*    Public Function Declarations                                           */
+/* ***************************************************************************/
+
+/**
+ * @brief check if char is a letter
+ *
+ * @param[in] pCommandTable - pointer to command table instance
+ * @param[in] numCommands - number of commands in table
+ */
+void uCli_init( uCli_command_t* pCommandTable, uint8_t numCommands );
+
+/**
+ * @brief process string input
+ *
+ * @param[in] string - string input
+ */
+void uCli_inputProcess( const char* string );
+
+
+/**
+ * @brief check if char word is a word
+ *
+ * @param[in] __char - char input
+ * @return true - char is a letter
+ * @return false - not a letter
+ */
+bool uCli_isWord( const char* string );
+
+/**
+ * @brief check if char word is a number
+ *
+ * @param[in] __char - char input
+ * @return true - char is a number
+ * @return false - not a number
+ */
+bool uCli_isNumeric( const char* string );
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif // __U_CLI_H
+
+/**
+  * @}
+  */
--- source/u_cli.c
+++ source/u_cli.c
@@ -9,3 +9,204 @@
  * @copyright Copyright (c) 2023
  * 
  */
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "tiny_cli.h"
+
+/* ***************************************************************************/
+/*    Private Defines                                                        */
+/* ***************************************************************************/
+/* ASCII delimiter character */
+#define DEL_CHAR                 ( ( char )0x08 )
+/* max number of args that can be registered */
+#define MAX_NUM_ARGS             ( 5 )
+/* check if character is a letter */
+#define IS_LETTER( c )           ( ( ( c >= 'a' ) || ( c <= 'z' ) ) || \
+                                   ( ( c >= 'A' ) || ( c <= 'Z' ) ) )
+/* check if character is a number */
+#define IS_NUMBER( c )             ( ( c >= '0' ) || ( c <= '9' ) )
+
+/* ***************************************************************************/
+/*    Private Type Defines                                                   */
+/* ***************************************************************************/
+
+/*!
+   @brief CLI command handler instance.
+ */
+typedef struct
+{
+    char          buf[MAX_RECEIVE_BUFFER_CHAR];
+    bool          busy;
+    uint8_t       numFill;
+    char          printBuf[MAX_PRINT_BUFFER_CHAR];
+    uCli_command_t* pCmdTable;
+    uint8_t       numCommands;
+} uCli_instance_t;
+
+/* ***************************************************************************/
+/*    Private Variables                                                      */
+/* ***************************************************************************/
+
+/* singleton instance */
+uCli_instance_t m_cli_instance;
+
+/* ***************************************************************************/
+/*    Private Function Definitions                                           */
+/* ***************************************************************************/
+
+/**
+ * @brief echo characters
+ *
+ * @param[in] __char - char input
+ */
+bool print( char* msg, ... )
+{
+    bool overflow = false;
+    va_list vList;
+
+    va_start( vList, msg );
+    overflow = ( MAX_PRINT_BUFFER_CHAR == vsnprintf(  m_cli_instance.printBuf, sizeof( m_cli_instance.printBuf ), msg, vList ) );
+    va_end( vList );
+    
+    printf( "%s", m_cli_instance.printBuf );
+    return overflow;
+}
+
+/**
+ * @brief check if char is a letter
+ *
+ * @param[in] pInstance - pointer to cli instance
+ * 
+ */
+void parse( uCli_instance_t* pInstance )
+{
+    char* cmd;
+    char* args[MAX_NUM_ARGS];
+    char* sep = " ";
+    uint8_t numArgs = 0;
+
+    // get tokens
+    cmd = strtok( pInstance->buf, sep );
+    if( NULL != cmd )
+    {
+        for( numArgs = 0; numArgs < MAX_NUM_ARGS; numArgs++ )
+        {
+            args[numArgs] = strtok( NULL, sep );
+            if( args[numArgs] == NULL )
+            {
+                break;
+            }
+        }
+
+        for( uint8_t i = 0; i < MAX_NUM_COMMANDS; i++ )
+        {
+            if( !strcmp( m_cli_instance.pCmdTable[i].cmd, cmd ) || 
+                !strcmp( m_cli_instance.pCmdTable[i].alias, cmd ) && 
+                ( NULL != m_cli_instance.pCmdTable[i].pCallback ) )
+            {
+                // found by command and alias
+                pInstance->pCmdTable[i].pCallback( &pInstance->pCmdTable[i], args, numArgs );
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * @brief check if char is a letter
+ *
+ * @param[in] pInstance - pointer to cli instance
+ * @param[in] __char - char input
+ */
+void input( uCli_instance_t* pInstance, char const __char )
+{
+    if( ( __char == '\b' ) || ( __char == ( ( char )0x7F ) ) )
+    {
+        if( pInstance->numFill > 0 )
+        {
+            // backspace was pressed, delete a char from buffer
+            pInstance->buf[pInstance->numFill--] = '\0';
+        }
+        // echo backspace
+        print( "%c", __char );
+    }
+    
+    else if( __char == '\r' ) // enter was pressed
+    {
+        // process input
+        parse( pInstance );
+        // clear buffer, prepare next receive
+        memset( pInstance->buf, '\0', MAX_RECEIVE_BUFFER_CHAR );
+        pInstance->numFill = 0;
+    }
+    
+    else // fill up the buffer
+    {
+        pInstance->buf[pInstance->numFill] = __char;
+        if( pInstance->numFill <= MAX_RECEIVE_BUFFER_CHAR )
+        {
+            pInstance->numFill += 1;    
+        }
+        else
+        {
+            pInstance->numFill = 0;
+        }
+    }
+}
+
+/* ***************************************************************************/
+/*    Private Function Definitions                                           */
+/* ***************************************************************************/
+
+bool uCli_isWord( const char* string )
+{
+    while( '\0' != *string )
+    {
+        if( !IS_LETTER( *( string++ ) ) )
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool uCli_isNumeric( const char* string )
+{
+    while( '\0' != *string )
+    {
+        if( !IS_NUMBER( *( string++ ) ) )
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void uCli_inputProcess( const char* string )
+{
+    while( '\0' != *string )
+    {
+        input( &m_cli_instance, *( string++ ) );
+    }
+}
+
+bool uCli_init( uCli_command_t* pCommandTable, uint8_t numCommands )
+{
+    if( ( NULL == pCommandTable ) || ( numCommands == 0 ) )
+    {
+        return false;
+    }
+    if( ( NULL != m_cli_instance->pCmdTable ) )
+    {
+        return false;
+    }
+    memset( &m_cli_instance, 0, sizeof( m_cli_instance ) );
+    m_cli_instance.pCmdTable = pCommandTable;
+    m_cli_instance.numCommands = numCommands;
+
+    return true;
+}
